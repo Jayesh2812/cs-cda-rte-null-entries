8e6dda0181fc2051d520602007d88445607e4da4
Jayesh Deorukhkar <jddeorukhkar@gmail.com>
2025-09-23 19:53:24 +0530

    basic app to get null entries

---
+++ src/utils/index.ts
@@ -0,0 +1,241 @@
+import contentstack, {
+  Region,
+  type StackConfig,
+} from "@contentstack/delivery-sdk";
+
+import get from "lodash.get";
+
+const REGION_CDN_URL_MAP = {
+  NA: "https://cdn.contentstack.io",
+  [Region.EU]: "https://eu-cdn.contentstack.com",
+  [Region.AU]: "https://au-cdn.contentstack.com",
+  [Region.AZURE_NA]: "https://azure-na-cdn.contentstack.com",
+  [Region.AZURE_EU]: "https://azure-eu-cdn.contentstack.com",
+  [Region.GCP_NA]: "https://gcp-na-cdn.contentstack.com",
+  [Region.GCP_EU]: "https://gcp-eu-cdn.contentstack.com",
+};
+
+export const getCDANullEntries = async (
+  region: Region,
+  accessToken: string,
+  branchName: string,
+  environment: string,
+  apiKey: string,
+  setMessage: (message: string) => void
+) => {
+  const stackConfig: StackConfig = {
+    accessToken,
+    branch: branchName,
+    environment,
+    apiKey,
+    deliveryToken: accessToken,
+  };
+
+  if (region) {
+    stackConfig.region = region;
+  }
+
+  const stack = contentstack.stack(stackConfig);
+
+  const contentTypes = (
+    await stack.contentType().includeGlobalFieldSchema().find()
+  ).content_types as any[];
+  const locales = (await getLocales(region, branchName, accessToken, apiKey));
+  let affectedEntriesSet = new Set();
+
+  if (!contentTypes || !locales) {
+    return {
+      success: false,
+      message: "Content types or locales not found",
+    };
+  }
+
+  for (const contentType of contentTypes) {
+    setMessage(`Processing contentType: ${contentType.uid}`);
+    for (const locale of locales) {
+      setMessage(`Processing locale ${locale.code} of contentType ${contentType.uid}`);
+      stack.setLocale(locale.code);
+      const paths = getAllRtePaths(contentType.schema);
+
+      let query = getQuery(paths);
+      for (const locale of locales) {
+        const entries = (
+          await stack.contentType(contentType.uid).entry().query(query).find()
+        ).entries as any[];
+
+        for (const entry of entries) {
+          if (entry.locale !== locale.code) {
+            continue;
+            // console.log(entry.uid, "Expected" ,locale.code, "Actual", entry.locale);
+          }
+          const absolutePaths = getAllAbsoluteJsonRtePaths(
+            contentType.schema,
+            entry
+          );
+          const nullPaths = absolutePaths.filter((path) => !get(entry, path));
+          if (nullPaths.length) {
+            affectedEntriesSet.add(
+              `${entry.uid} - ${contentType.uid} - ${entry.locale} - ${entry._version}`
+            );
+          }
+        }
+      }
+    }
+  }
+
+  setMessage("Entries fetched successfully");
+  return {
+    success: true,
+    message: "Entries fetched successfully",
+    affectedEntriesSet,
+  };
+};
+
+async function getLocales(
+  region: Region,
+  branch: string,
+  access_token: string,
+  api_key: string
+) {
+  const API_URL =
+    REGION_CDN_URL_MAP[(region as keyof typeof REGION_CDN_URL_MAP) || "NA"];
+  const url = `${API_URL}/v3/locales`;
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      access_token,
+      api_key,
+      branch,
+    },
+  });
+  return response.json().then((locales) => locales.locales);
+}
+
+function getAllRtePaths(schema: any, parentPath = ""): string[] {
+  const paths = [];
+  for (const field of schema) {
+    const fieldUid = field.uid;
+
+    const isRte = field.field_metadata?.allow_json_rte;
+    const isGroupOrGlobal = ["group", "global_field"].includes(field.data_type);
+    const isModularBlocks = field.data_type === "blocks";
+
+    if (isRte) {
+      paths.push(getFieldPath(parentPath, fieldUid));
+    } else if (isGroupOrGlobal) {
+      paths.push(
+        ...getAllRtePaths(field.schema, getFieldPath(parentPath, fieldUid))
+      );
+    } else if (isModularBlocks) {
+      const blockSchemaMap = Object.fromEntries(
+        field.blocks.map((block: any) => [block.uid, block.schema])
+      );
+      for (const block of field.blocks) {
+        if (block.uid)
+          paths.push(
+            ...getAllRtePaths(
+              blockSchemaMap[block.uid],
+              [parentPath, fieldUid, block.uid].filter((i) => i).join(".")
+            )
+          );
+      }
+    }
+  }
+  return paths;
+}
+
+function getFieldPath(parentPath: string, fieldUid: string, index = "") {
+  const pathArray = [];
+
+  if (parentPath) pathArray.push(parentPath);
+  pathArray.push(fieldUid);
+  if (index) pathArray.push(index);
+
+  return pathArray.join(".");
+}
+
+const getQuery = (paths: string[]) => {
+  const pathQuery = paths.map((path) => {
+    return {
+      $and: [
+        { [path]: { $exists: true } },
+        { [path + ".uid"]: { $exists: false } },
+      ],
+    };
+  });
+
+  return {
+    $or: [...pathQuery],
+  };
+};
+
+function getAllAbsoluteJsonRtePaths(
+  schema: any[],
+  entry: any,
+  parentPath = ""
+): string[] {
+  const paths: string[] = [];
+  if (!schema || !entry) return paths;
+  for (const field of schema) {
+    const fieldUid = field.uid;
+    const fieldValue = entry[fieldUid];
+
+    const isMultiple = field.multiple;
+    const isJsonRte =
+      field.data_type === "json" && field.field_metadata?.allow_json_rte;
+    const isGroupOrGlobal = ["group", "global_field"].includes(field.data_type);
+    const isModularBlocks = field.data_type === "blocks";
+    let index;
+
+    if (!fieldValue && !isJsonRte) continue;
+
+    if (isJsonRte) {
+      if (isMultiple) {
+        for (index of Object.keys(fieldValue)) {
+          paths.push(getFieldPath(parentPath, fieldUid, index));
+        }
+      } else {
+        paths.push(getFieldPath(parentPath, fieldUid, index));
+      }
+    } else if (isGroupOrGlobal) {
+      if (isMultiple) {
+        for (index of Object.keys(fieldValue)) {
+          paths.push(
+            ...getAllAbsoluteJsonRtePaths(
+              field.schema,
+              fieldValue[index],
+              getFieldPath(parentPath, fieldUid, index)
+            )
+          );
+        }
+      } else {
+        paths.push(
+          ...getAllAbsoluteJsonRtePaths(
+            field.schema,
+            fieldValue,
+            getFieldPath(parentPath, fieldUid, index)
+          )
+        );
+      }
+    } else if (isModularBlocks) {
+      const blockSchemaMap = Object.fromEntries(
+        field.blocks.map((block: any) => [block.uid, block.schema])
+      );
+      for (index of Object.keys(fieldValue)) {
+        const block = fieldValue[index];
+        const [block_uid] = Object.keys(block);
+        if (block_uid)
+          paths.push(
+            ...getAllAbsoluteJsonRtePaths(
+              blockSchemaMap[block_uid],
+              fieldValue[index][block_uid],
+              [parentPath, fieldUid, index, block_uid]
+                .filter((i) => i)
+                .join(".")
+            )
+          );
+      }
+    }
+  }
+  return paths;
+}
